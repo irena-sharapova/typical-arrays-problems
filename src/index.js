96b8848702b58503c1d09a9603216d5dca877b76
Irina Sharapova <ir.ur.smirnova@gmail.com>
2020-10-02 23:29:46 -0400

    fiat: add new file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0)
+    return 0;
+  
+  return Math.min(...array);
 }
+  
+  
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0)
+    return 0;
+
+  return Math.max(...array);
 }
+  
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0)
+    return 0;
+
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum / array.length;
 }
